be2dabf8c6aa1b2b89d770bf326eae693cf94290
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-05-13 20:45:14 -0300

    Chore: Estrutura do Projeto

---
+++ src/main/java/com/contrucontrol/contrucontrol/ContrucontrolApplication.java
@@ -0,0 +1,13 @@
+package com.contrucontrol.contrucontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrucontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrucontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contrucontrol/contrucontrol/ContrucontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contrucontrol.contrucontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrucontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
