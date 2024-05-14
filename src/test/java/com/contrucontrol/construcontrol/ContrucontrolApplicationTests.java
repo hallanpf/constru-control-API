7c43bc6fab7c592eb54c37959679ef702e5f4cda
gabriel marques <gabrielmarksf@icloud.com>
2024-05-14 13:15:22 -0300

    feat: add config h2 database

--- src/test/java/com/contrucontrol/construcontrol/ContrucontrolApplicationTests.java
+++ src/test/java/com/contrucontrol/construcontrol/ContrucontrolApplicationTests.java
@@ -1,4 +1,4 @@
-package com.contrucontrol.contrucontrol;
+package com.contrucontrol.construcontrol;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
