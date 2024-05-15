ff58b3aee0d8d3d0c2ac810c320e698a5961ee01
gabriel marques <gabrielmarksf@icloud.com>
2024-05-14 21:47:03 -0300

    feat: create user class, repository and controller

---
+++ src/main/java/com/construcontrol/construcontrol/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.construcontrol.construcontrol.controllers;
+
+import com.construcontrol.construcontrol.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @GetMapping
+  public ResponseEntity getAllUsers() {
+    var allUsers = userRepository.findAll();
+    return ResponseEntity.ok(allUsers);
+  }
+
+}
