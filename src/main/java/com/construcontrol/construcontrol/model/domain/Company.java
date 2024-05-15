41fc87fd13e9e4ffc205c3a7cf0ad9c5bce68995
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-05-14 23:51:07 -0300

    developer

--- src/main/java/com/construcontrol/construcontrol/model/domain/Company.java
+++ src/main/java/com/construcontrol/construcontrol/model/domain/Company.java
@@ -1,23 +1,22 @@
 package com.construcontrol.construcontrol.model.domain;
-
 import lombok.*;
 import jakarta.persistence.*;
 
-@GetterS
+@Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualAndHashCode(of = "id")
 @Entity
 @Table(name = "companies")
 public class Company {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+
     private long id;
     @Column(name = "company")
     protected String company;
     @Column(name = "cnpj")
     protected String cnpj;
     @JoinColumn(name = "address_id", referencedColumnName = "id")
+      @OneToOne
     private Address address;
 }
