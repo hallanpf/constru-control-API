970574bb7cb79606db31c4e65a8523506af6dfa1
Guilherme de Medeiros Ferreira <g_medeiros@outlook.com.br>
2024-05-20 22:19:39 -0300

    Create class Apartament

---
+++ src/main/java/com/construcontrol/construcontrol/model/domain/projects/Apartament.java
@@ -0,0 +1,37 @@
+package com.construcontrol.construcontrol.model.domain.projects;
+
+import com.construcontrol.construcontrol.DTO.projects.ApartamentDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "apartaments")
+public class Apartament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "construction_id", nullable = false, referencedColumnName = "id")
+//    private Construction construction;
+    @Column(name = "number")
+    private int number;
+    @Column(name = "area")
+    private double area;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "sold_status")
+    private boolean sold = false;
+
+    public Apartament(ApartamentDTO apartamentDTO) {
+//        this.construction = apartamentDTO.construction;
+        this.number = apartamentDTO.number();
+        this.area = apartamentDTO.area();
+        this.price = apartamentDTO.price();
+        this.sold = apartamentDTO.sold();
+    }
+}
---
+++ src/main/java/com/construcontrol/construcontrol/repositories/projects/ApartamentRepository.java
@@ -0,0 +1,13 @@
+package com.construcontrol.construcontrol.repositories.projects;
+
+import com.construcontrol.construcontrol.model.domain.projects.Apartament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ApartamentRepository extends JpaRepository<Apartament, Long> {
+    Apartament getApartamentById(long id);
+    Optional<Apartament> getApartamentBySold(Boolean soldStatus);
+
+    Apartament deleteApartament(long id);
+    Optional<Apartament> deleteApartamentBySold(Boolean soldStatus);
+}
