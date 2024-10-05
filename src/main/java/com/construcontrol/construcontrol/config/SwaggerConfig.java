71f06dedfc527c074b313e7b936994f0ae53878a
gabriel marques <gabrielmarksf@icloud.com>
2024-10-05 13:27:02 -0300

    fix: atualizando config de auth

--- src/main/java/com/construcontrol/construcontrol/config/SwaggerConfig.java
+++ src/main/java/com/construcontrol/construcontrol/config/SwaggerConfig.java
@@ -1,17 +1,27 @@
 package com.construcontrol.construcontrol.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    // Configuração do Swagger
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info().title("ConstruControl API").version("1.0.0")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
+  // Configuração do Swagger
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .components(new Components()
+                    .addSecuritySchemes("bearer-token",
+                            new SecurityScheme()
+                                    .type(SecurityScheme.Type.APIKEY)
+                                    .in(SecurityScheme.In.HEADER)
+                                    .name("Authorization"))).info(new Info()
+                    .title("ConstruControl API")
+                    .version("1.0.0"))
+            .addSecurityItem(new SecurityRequirement().addList("bearer-token"));
+  }
 }
--- src/main/java/com/construcontrol/construcontrol/infra/security/SecurityConfiguration.java
+++ src/main/java/com/construcontrol/construcontrol/infra/security/SecurityConfiguration.java
@@ -24,14 +24,16 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity.csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                    //permit all http://localhost:8080/swagger-ui/index.html
-                    .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-ui/index.html").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-          .requestMatchers(HttpMethod.POST, "/companies", "/constructions", "/employees", "/suppliers", "/clients", "/brokers", "/manager").hasRole("GESTOR") // autorizando apenas o gestor a fazer requests nesses endpoints.
-            .anyRequest().authenticated())
+                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/companies", "/constructions", "/employees", "/suppliers", "/clients", "/brokers", "/manager").hasRole("GESTOR")
+                    .anyRequest().authenticated()
+            )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
   }
